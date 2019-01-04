d03e66596c83ac1eee4466520c8058cb671ce9b5
Joshua Sucsy <jsucsy@gmail.com>
2019-01-03 22:05:37 -0500

    rolled back to working version, commented out lineParse function stub

--- spruce/PageRank.kt
+++ spruce/PageRank.kt
@@ -1,8 +1,11 @@
 import java.io.File
 
-fun lineParse(line: String): Array<String> {
-  return 
-}
+/** Next step: work out the line parser - return array of values
+ *
+ *fun lineParse(line: String): Array<String> {
+ * return 
+ *}
+*/
 
 fun main(args: Array<String>) {
   File("log.csv").forEachLine { println(it) }

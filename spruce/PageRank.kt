4be3a8274b450e272697f94a4df09527fc007d81
Joshua Sucsy <jsucsy@gmail.com>
2019-01-05 11:29:52 -0500

    corrected code style per ktlint

--- spruce/PageRank.kt
+++ spruce/PageRank.kt
@@ -1,43 +1,34 @@
-/** TODO: 
- *
- */
-
 package spruce.logmetrics
 import java.io.File
 
-data class Hit(val path: String, val user: String, val timestamp: String) { 
-}
+data class Hit(val path: String, val user: String, val timestamp: String)
 
-fun openLog(logfile: String): List<String>{
-  // read file, confirm format, and drop header
-  val allLines = File(logfile).bufferedReader().readLines() 
-  val header = allLines[0]
-  
-  // error checks - I would expect to expand this based on testing
-  if (header != "Path,User,Timestamp") { 
-    println("***** WARNING: UNEXPECTED LOG FORMAT *****")
-    println("***** WARNING: UNEXPECTED LOG FORMAT *****")
-    println("Expected format is (Path,User,Timestamp), got $header")
-    // val filepath = File(logfile).getPath()
-    println("Please confirm that log file $logfile is correct")
-    println("***** WARNING: UNEXPECTED LOG FORMAT *****")
-    println("***** WARNING: UNEXPECTED LOG FORMAT *****\n\n")
-  }
-  return allLines.drop(1)
+fun openLog(logfile: String): List<String> {
+    // read file, run error checks, and drop header
+    val allLines = File(logfile).bufferedReader().readLines()
+    val header = allLines[0]
+    // error checks - I would expect to expand this based on testing
+    if (header != "Path,User,Timestamp") {
+        println("***** WARNING: UNEXPECTED LOG FORMAT *****")
+        println("***** WARNING: UNEXPECTED LOG FORMAT *****")
+        println("Expected format is (Path,User,Timestamp), got $header")
+        // val filepath = File(logfile).getPath()
+        println("Please confirm that log file $logfile is correct")
+        println("***** WARNING: UNEXPECTED LOG FORMAT *****")
+        println("***** WARNING: UNEXPECTED LOG FORMAT *****\n\n")
+    }
+    return allLines.drop(1)
 }
 
 fun lineParse(line: String): Hit {
-  val fields = line.split(",")
-  val hit = Hit(fields[0], fields[1], fields[2])
-  return hit
+    val fields = line.split(",")
+    val hit = Hit(fields[0], fields[1], fields[2])
+    return hit
 }
 
-
 fun main(args: Array<String>) {
-  val logfile = "log.csv"
-  val lines = openLog(logfile)
-  for (line in lines) {
-    val parsedLine = lineParse(line)
-    println(parsedLine)
-  }
+    val logfile = "log.csv"
+    val lines = openLog(logfile)
+    val hits = lines.map { lineParse(it) }
+    println(hits)
 }
--- spruce/readme_kotlin.txt
+++ spruce/readme_kotlin.txt
@@ -8,3 +8,25 @@ Compile - library: kotlinc hello.kt -d hello.jar
 Run: java -jar hello.jar
 
 REPL: kotlinc-jvm
+
+use ktlint to check code
+# check the style of all Kotlin files inside the current dir (recursively)
+# (hidden folders will be skipped)
+$ ktlint --color
+  src/main/kotlin/Main.kt:10:10: Unused import
+  
+# check only certain locations (prepend ! to negate the pattern) 
+$ ktlint "src/**/*.kt" "!src/**/*Test.kt"
+
+# auto-correct style violations
+# (if some errors cannot be fixed automatically they will be printed to stderr) 
+$ ktlint -F "src/**/*.kt"
+
+# print style violations grouped by file
+$ ktlint --reporter=plain?group_by_file
+# print style violations as usual + create report in checkstyle format 
+$ ktlint --reporter=plain --reporter=checkstyle,output=ktlint-report-in-checkstyle-format.xml
+
+# install git hook to automatically check files for style violations on commit
+# use --install-git-pre-push-hook if you wish to run ktlint on push instead
+$ ktlint --install-git-pre-commit-hook

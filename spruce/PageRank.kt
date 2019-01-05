df2148b794b21cc2c7ed64560b07aa488b6c2ca0
Joshua Sucsy <jsucsy@gmail.com>
2019-01-05 13:49:01 -0500

    converted timestamp from String to ZonedDateTime, grouped by ChronoUnit.DAYS

--- spruce/PageRank.kt
+++ spruce/PageRank.kt
@@ -1,7 +1,9 @@
 package spruce.logmetrics
 import java.io.File
+import java.time.ZonedDateTime
+import java.time.temporal.ChronoUnit
 
-data class Hit(val path: String, val user: String, val timestamp: String)
+data class Hit(val path: String, val user: String, val timestamp: ZonedDateTime)
 
 fun openLog(logfile: String): List<String> {
     // read file, run error checks, and drop header
@@ -22,7 +24,8 @@ fun openLog(logfile: String): List<String> {
 
 fun lineParse(line: String): Hit {
     val fields = line.split(",")
-    val hit = Hit(fields[0], fields[1], fields[2])
+    val ts = ZonedDateTime.parse(fields[2])
+    val hit = Hit(fields[0], fields[1], ts)
     return hit
 }
 
@@ -30,8 +33,14 @@ fun calcMetrics(hits: List<Hit>) {
     // produce by-day rankings of:
     // - pages by number of users
     // - users by unique page views
-    val byPath: Grouping<Hit, String> = hits.groupingBy { it.path }
-    println(byPath.eachCount())
+
+    val groupByDate = hits.groupBy { it -> it.timestamp.truncatedTo(ChronoUnit.DAYS) }
+        // .groupingBy { it.timestamp.truncatedTo(ChronoUnit.DAYS) }
+    groupByDate.forEach { println(it) }
+    
+    // TODO: map/filter/reduce groupByDate to pages by users and users by unique views 
+    // val pagesByUsers = groupByDate.mapValues { (path, user) -> user.count() { it } }
+    // pagesByUsers.forEach { println(it) }
 }
 
 fun main(args: Array<String>) {
--- spruce/readme_kotlin.txt
+++ spruce/readme_kotlin.txt
@@ -1,11 +1,11 @@
-Help:   kotlinc -help
+Full build w/ ktlint check: ./buildpr.sh
 
+Run: java -jar hello.jar
+
+Help:   kotlinc -help
 
 Compile - runnable: kotlinc PageRank.kt -include-runtime -d PageRank.jar
 Compile - library: kotlinc hello.kt -d hello.jar
-
-
-Run: java -jar hello.jar
 
 REPL: kotlinc-jvm
 

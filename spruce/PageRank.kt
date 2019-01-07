aff9dcaab4a4f7fb48f275eb608062198fa2ca94
Joshua Sucsy <jsucsy@gmail.com>
2019-01-07 15:19:38 -0500

    code cleanup for delivery

--- spruce/PageRank.kt
+++ spruce/PageRank.kt
@@ -3,6 +3,13 @@ import java.io.File
 import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit
 
+/**
+ * Data class for page hits from HTTP log
+ * @param path: HTTP path of page visited
+ * @param user: USERID visiting page
+ * @param timestamp: ZonedDateTime timestamp of page visit
+ * @param date: ZonedDateTime truncated date-only timestamp to speed up date grouping/filtering
+ */
 data class Hit(
     val path: String,
     val user: String,
@@ -10,6 +17,12 @@ data class Hit(
     val date: ZonedDateTime
 )
 
+/**
+ * Opens HTTP log
+ * Reads file and runs error checks
+ * @param logfile: path to log file
+ * @return a List<String> of all lines except 1-line header
+ */
 fun openLog(logfile: String): List<String> {
     // read file, run error checks, and drop header
     val allLines = File(logfile).bufferedReader().readLines()
@@ -26,6 +39,11 @@ fun openLog(logfile: String): List<String> {
     return allLines.drop(1)
 }
 
+/**
+ * Parses line from HTTP log into Hit data class
+ * @param line: line from HTTP log
+ * @return hit: page visit data in Hit class format
+ */
 fun lineParse(line: String): Hit {
     val fields = line.split(",")
     val ts = ZonedDateTime.parse(fields[2])
@@ -34,10 +52,16 @@ fun lineParse(line: String): Hit {
     return hit
 }
 
+/**
+ * Calculate and print metrics of HTTP log
+ * produce by-day rankings of:
+ * - pages by number of users: counts total hits for each page regardless of duplicates hits by user
+ * - users by unique page views: ERROR:: should count unique views, currently counts duplicates
+ * @param hitList: HTTP log in List<Hit> format
+ * @return currently returns nothing, all output is printed directly
+ * A full production implementation would account for the intended use of this data
+ */
 fun calcMetrics(hitList: List<Hit>) {
-    // produce by-day rankings of:
-    // - pages by number of users
-    // - users by unique page views
     val groupHitsByDate = hitList.groupBy { it -> it.date }
     val dateKeys = groupHitsByDate.keys
     for (key in dateKeys) {
@@ -54,6 +78,7 @@ fun calcMetrics(hitList: List<Hit>) {
     }
 }
 
+/** Main program entry */
 fun main(args: Array<String>) {
     val logfile = "log.csv"
     val lines = openLog(logfile)
---
+++ spruce/readme.md
@@ -0,0 +1,44 @@
+* Project Status
+This project currently returns an incorrect metric and needs to be fixed.  It is not yet ready for production.  The "users by unique page views" metric displays by total views, not unique views.  Also, functionality testing needs to be expanded and automated into the build process.
+
+* File list:
+- buildpr.sh:     bash script to check code syntax and compile binary
+- PageRank.jar    compiled binary
+- PageRank.kt     source code
+- readme.md       documentation (this file)
+- log.csv         original (provided) HTTP log
+- log_test.csv    log with dummy values for testing - rename to log.csv to test
+
+* STATED ASSUMPTIONS 
+- I'm simply printing my output.  In a production run, I would either consume the results with another script or add a database connection directly.
+- File size is not a significant fraction of available memory.  I would likely split the logs if this was a production issue.
+
+* Project Definition
+"Treat it as production level code, upload to Github when complete, include a brief README, and ping me.
+
+Please complete the below problem for an arbitrary production HTTP server's log.  The only restriction is that you use Kotlin as your language of choice.
+
+Given an [HTTP server access log]
+(i.e. https://gist.github.com/zvozin/88fe5e242118efba48b66c43806da6ef), 
+produce by-day rankings of:
+- pages by number of users
+- users by unique page views
+
+
+Do think through the performance/maintainability implications of the choices you're making."
+
+* PROJECT PLAN
+1.  DONE pull the file
+2.  DONE get the file open with kotlin
+3.  DONE parse the string
+4.  DONE create data class for hits
+5.  WIP W/ERRORS produce output metrics
+6.  DONE create dummy log for testing
+7.  DONE write error checks
+8.  DONE Document code using KDoc syntax
+9.  DONE Automate syntax checks
+10. DONE Automate build
+11. DONE Finalize deliverables
+12. DONE Upload to Github
+13. DONE Deliver NLT 17:00 01-07-2019 Monday
+
